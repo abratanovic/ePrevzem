bed063f8e23e7149a7b125216ba031d555a06670
Adnan Bratanović <adnan.bratanovic@windowslive.com>
2026-05-17 15:11:45 +0200

    fixed stale tests in eosebna

--- sitrust-mock/eosebna_mobile/test/screens/home_screen_test.dart
+++ sitrust-mock/eosebna_mobile/test/screens/home_screen_test.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:eosebna_mobile/app/app.dart';
 
 void main() {
