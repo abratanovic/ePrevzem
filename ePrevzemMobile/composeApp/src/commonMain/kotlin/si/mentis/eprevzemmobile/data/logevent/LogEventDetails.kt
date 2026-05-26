e00815dd0cba5a881a8f796ba120021f784f1c9a
emir <ribicemir15@gmail.com>
2026-05-26 13:06:36 +0200

    PRVZM-35

---
+++ ePrevzemMobile/composeApp/src/commonMain/kotlin/si/mentis/eprevzemmobile/data/logevent/LogEventDetails.kt
@@ -0,0 +1,8 @@
+package si.mentis.eprevzemmobile.data.logevent
+
+data class LogEventDetails(
+    val documentTitle: String? = null,
+    val organizationName: String? = null,
+    val lockerLabel: String? = null,
+    val location: String? = null,
+)
