00814da282c1784d20d013a16e7335ae2268f700
emir <ribicemir15@gmail.com>
2026-05-31 16:08:58 +0200

    PRVZM-57: backend connected

--- frontend/src/services/stations/stationService.ts
+++ frontend/src/services/stations/stationService.ts
@@ -1,6 +1,3 @@
 import { httpStationAdapter } from "./httpStationAdapter";
-import { mockStationAdapter } from "./mockStationAdapter";
 
-const useMockStations = import.meta.env.VITE_USE_MOCK_STATIONS !== "false";
-
-export const stationService = useMockStations ? mockStationAdapter : httpStationAdapter;
+export const stationService = httpStationAdapter;
